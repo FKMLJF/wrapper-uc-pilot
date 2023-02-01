7e4dc01d3d66475524f5400820a8a06eb701c624
hubbabubb <gergohuber@gmail.com>
2023-02-01 16:41:39 +0100

    Add QR Reader to project

--- App.tsx
+++ App.tsx
@@ -2,27 +2,124 @@
  * Copyright (c) 2023 Jozsef Vincze
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {
+    Linking,
+    StyleSheet,
+    View,
+} from 'react-native';
 import { WebView } from 'react-native-webview';
 
-function App(): JSX.Element {
-  const URL = 'https://uc.procats.hu';
+import { RNCamera } from 'react-native-camera';
 
-  return (
-      <SafeAreaView style={styles.body}>
-        <WebView source={{uri: URL}}></WebView>
-      </SafeAreaView>
-  );
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function App(): JSX.Element {
+    const Stack = createNativeStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Uril Card Page">
+                    {(props) => <UrilCardPage {...props} />}
+                </Stack.Screen>
+                <Stack.Screen name="CodeReader" component={QRCodeReader} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+function UrilCardPage(navigation: any): JSX.Element {
+    const URL = 'https://uc.procats.hu';
+    const webView = useRef<WebView>(null);
+
+    const readQrCode = async () => {
+        navigation.navigate('CodeReader')
+    }
+
+    const readNFCTag = async () => {
+        // do something
+    }
+
+    return (
+        <View style={styles.body}>
+            <WebView
+                source={{uri: URL}}
+                ref={ webView }
+                onLoadEnd={ () => webView.current?.postMessage("webViewLoaded") }
+                onMessage={ (event) => {
+                    switch (event.nativeEvent.data) {
+                        case "read-to-qr-code" :
+                            readQrCode().then(r => {});
+                            break;
+                        case "read-to-nfc-tag" :
+                            readNFCTag().then(r => {});
+                            break;
+                        default:
+                            break;
+                    }
+                }}
+            ></WebView>
+        </View>
+    )
+}
+
+function QRCodeReader(): JSX.Element {
+    const onSuccess = (e: any) => {
+        Linking.openURL(e.data).catch(err =>
+            console.error('An error occured', err)
+        );
+    };
+
+    return (
+        <View style={styles.body}>
+            <RNCamera
+                onBarCodeRead={ onSuccess.bind(this) }
+                style={styles.preview}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                    title: 'Permission to use audio recording',
+                    message: 'We need your permission to use your audio',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}>
+            </RNCamera>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    backgroundColor: 'black',
-    color: 'white',
-  },
+    body: {
+        flex: 1,
+        backgroundColor: 'black',
+        color: 'white',
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    }
 });
 
 export default App;
